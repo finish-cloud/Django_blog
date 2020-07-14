779d34a78fb8f8aaca05b3aaafd750700dee0363
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2020-07-14 13:01:14 +0900

    delete secret

--- dbsite/settings.py
+++ dbsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_t!u7*ylkjp9u&$9@p5(!0b$4$#y2gr014ur-2ufv$v_(sts=r'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
